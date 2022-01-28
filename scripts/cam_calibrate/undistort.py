4f9401bf9bafca0d4730d699270b95c0cf2efe7b
B33Boy <abhip202@gmail.com>
2022-01-28 17:15:16 -0500

    hotfix: cleaned up undistort.py

--- scripts/cam_calibrate/undistort.py
+++ scripts/cam_calibrate/undistort.py
@@ -3,36 +3,32 @@ import cv2
 
 # https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
 
-# Load previously saved data
-with np.load('res/cal_out/cam_params.npz') as X:
-    mtx, dist, rvecs, tvecs = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
-    print(mtx.shape)
-    print(dist.shape)
-    print(rvecs.shape)
-    print(tvecs.shape)
-    print("-------------------------")
+
 
 def undistort_camera(img, mtx, new_mtx, roi, dist, w, h):
 
     # undistort
     dst = cv2.undistort(img, mtx, dist, None, new_mtx)
+    
     # crop the image
     x, y, w, h = roi
-    # return dst[y:y+h, x:x+w]
+    return dst[y:y+h, x:x+w]
 
 
+# Load camera calibration data from cam_out folder
+with np.load('res/cal_out/cam_params.npz') as X:
+    mtx, dist, rvecs, tvecs = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
 
+
+# Start the video capture
 cam = cv2.VideoCapture(0)
 
+# Obtain the width and height of the camera
 w = cam.get(cv2.CAP_PROP_FRAME_WIDTH )
 h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT )
-print("height=", h, " width=", w)
 
 # Undistort Camera Matrix + ROI
 new_mtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-print(new_mtx)
-print(roi)
-print("=======================")
 
 
 while True:
@@ -45,17 +41,11 @@ while True:
     # Rotate image 180 degrees
     frame = cv2.rotate(frame, cv2.ROTATE_180)
 
-    # # First undistort
-    # undist_frame = undistort_camera(frame, mtx, new_mtx, roi, dist, w, h)
-    # print("Shape of undistorted frame: ",undist_frame.shape)
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-    dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
-    # crop the image
-    x, y, w, h = roi
-    dst = dst[y:y+h, x:x+w]
+    # First undistort
+    undist = undistort_camera(frame, mtx, new_mtx, roi, dist, w, h)
 
     cv2.imshow("distorted", frame)
-    cv2.imshow("undistorted", dst)
+    cv2.imshow("undistorted", undist)
 
     k = cv2.waitKey(1)
 
