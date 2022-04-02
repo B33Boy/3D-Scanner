2ea8828c063be4264a825c024c1d456a16dc2cd8
B33Boy <abhip202@gmail.com>
2022-04-02 18:20:34 +0000

    chore: fixing program flow

--- Project/run2.py
+++ Project/run2.py
@@ -364,15 +364,16 @@ def main():
         # quitting button you may use any
         # desired button of your choice
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            onFlag = False
-            scanFlag = False
+            #onFlag = False
+            #scanFlag = False
+            
+            stopScan()
             break
     
     # After the loop release the cap object
     vid.release()
     # Destroy all the windows
     cv2.destroyAllWindows()
-
 
     print("Program Finished")
 
